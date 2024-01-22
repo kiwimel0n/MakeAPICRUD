e50b7b3d30190b98cfa2492296ca9cead6cbfa7f
kiwimel0n <alstkd8580@naver.com>
2024-01-22 19:14:17 +0900

    스케쥴 Entity수정

--- src/main/java/org/sparta/myschedulemanage/entity/Schedule.java
+++ src/main/java/org/sparta/myschedulemanage/entity/Schedule.java
@@ -1,14 +1,13 @@
 package org.sparta.myschedulemanage.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.type.descriptor.jdbc.NVarcharJdbcType;
+import lombok.*;
+import org.sparta.myschedulemanage.dto.ScheduleRequestDto;
 
 @Entity
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
 @Table(name = "schedule")
 @NoArgsConstructor
 public class Schedule {
@@ -22,5 +21,26 @@ public class Schedule {
     private String contents;
     @Column(name = "manager", nullable = false)
     private String manager;
+    @Column(name = "password", nullable = false)
+    private int password;
+
+
+
+
+    public static Schedule of(ScheduleRequestDto requestDto) {
+        return Schedule.builder()
+                .taskName(requestDto.taskName())
+                .contents(requestDto.contents())
+                .manager(requestDto.manager())
+                .password(requestDto.password())
+                .build();
+    }
+
+    public void update(ScheduleRequestDto requestDto) {
+        this.taskName = requestDto.taskName();
+        this.contents = requestDto.contents();
+        this.manager = requestDto.manager();
+    }
 
 }
+
