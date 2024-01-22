653baaf4f79ce59ddc54c53a5052e4c3dd28b9f4
kiwimel0n <alstkd8580@naver.com>
2024-01-22 15:07:48 +0900

    스케쥴 Entity생성

---
+++ src/main/java/org/sparta/myschedulemanage/MyScheduleManageApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.myschedulemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScheduleManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyScheduleManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/myschedulemanage/MyScheduleManageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.myschedulemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScheduleManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
