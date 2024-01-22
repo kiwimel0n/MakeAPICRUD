9c5d8ae0c7543274d95b55784019e514140888a7
kiwimel0n <alstkd8580@naver.com>
2024-01-22 19:14:59 +0900

    레파지토리 생성

--- src/main/java/org/sparta/myschedulemanage/repository/ScheduleRepository.java
+++ src/main/java/org/sparta/myschedulemanage/repository/ScheduleRepository.java
@@ -3,6 +3,11 @@ package org.sparta.myschedulemanage.repository;
 import org.sparta.myschedulemanage.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findAllByOrderByModifiedAtDesc();
 
 }
