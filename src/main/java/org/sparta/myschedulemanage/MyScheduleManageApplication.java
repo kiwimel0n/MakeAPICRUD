5b3bcc537f7c4b877f623c7555b1dc61ef6176f7
kiwimel0n <alstkd8580@naver.com>
2024-01-23 11:01:55 +0900

    Auditing 생성

--- src/main/java/org/sparta/myschedulemanage/MyScheduleManageApplication.java
+++ src/main/java/org/sparta/myschedulemanage/MyScheduleManageApplication.java
@@ -2,6 +2,7 @@ package org.sparta.myschedulemanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class MyScheduleManageApplication {
